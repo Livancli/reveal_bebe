02a7dff72749aee509bcab2f3dd58bf75b6efb0d
Silvestre <silvestrevideoproducoes@gmail.com>
2023-08-16 23:08:03 -0300

    start_reveal

---
+++ script.js
@@ -0,0 +1,26 @@
+const revealButton = document.getElementById('revealButton');
+const revealContainer = document.getElementById('revealContainer');
+const countdownElement = document.getElementById('countdown');
+const animationElement = document.getElementById('animation');
+const genderTextElement = document.getElementById('genderText');
+
+revealButton.addEventListener('click', () => {
+  revealButton.disabled = true;
+  revealContainer.classList.remove('hidden');
+  revealButton.classList.add('hidden');
+  let countdown = 5;
+  const countdownInterval = setInterval(() => {
+    countdown--;
+    countdownElement.innerText = countdown.toString();
+    if (countdown === 0) {
+      clearInterval(countdownInterval);
+      const gender = Math.random() < 0.5 ? 'Menino' : 'Menina';
+      // const gender = 'Menino';
+      genderTextElement.innerText = gender;
+      animationElement.innerText = gender === 'Menino' ? '👦Menino' : '👧Menina';
+      countdownElement.classList.add('hidden');
+      document.body.style.backgroundColor = gender === 'Menino' ? 'rgb(173, 214, 250)' : ' rgb(244, 205, 253)';
+    }
+  }, 1000);
+
+});
